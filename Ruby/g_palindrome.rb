c7cfa882bc3614051dda8cc1d6d15c9052eaadc8
Galiwango Ananiya <galiwango101@gmail.com>
2021-01-16 23:33:38 +0300

    add comment to test

--- Ruby/g_palindrome.rb
+++ Ruby/g_palindrome.rb
@@ -5,9 +5,8 @@ require 'rspec'
 def palindrome?(word)
   word.downcase == word.downcase.reverse
 end
-p palindrome?('tacocat')
-p palindrome?('Tacocat')
-p palindrome?('baseball')
+
+# Test method with rspec
 describe 'Check if a word is a palindrome' do
   it 'returns true if the word is a palindrome, false if its not' do
     expect(palindrome?('tacocat')).to be(true)
