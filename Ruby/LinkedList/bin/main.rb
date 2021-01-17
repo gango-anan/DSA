7f5546da8f8cd44a8b790abc5e6b15707a6587fa
Galiwango Ananiya <galiwango101@gmail.com>
2021-01-17 18:56:46 +0300

    fix rubocop errors

--- Ruby/LinkedList/bin/main.rb
+++ Ruby/LinkedList/bin/main.rb
@@ -1,32 +1,26 @@
+# frozen_string_literal: true
+
 require_relative '../lib/node'
 require_relative '../lib/linked_list'
 
-node_1 = Node.new('gango')
-node_2 = Node.new('anan')
-
-node_1.next_node = node_2
-node_3 = Node.new('max')
-node_2.next_node = node_3
-
-node_4 = Node.new('ananiya')
-node_3.next_node = node_4
-
-node_5 = Node.new('pioneer')
-node_4.next_node = node_5
-
-node_6 = Node.new('swaria')
-node_5.next_node = node_6
-
-node_7 = Node.new('moruapesur')
-node_6.next_node = node_7
-
-node_8 = Node.new('hilders')
-node_7.next_node = node_8
-
-node_9 = Node.new('madera')
-node_8.next_node = node_9
-
-list = LinkedList.new(node_1)
+node1 = Node.new('gango')
+node2 = Node.new('anan')
+node1.next_node = node2
+node3 = Node.new('max')
+node2.next_node = node3
+node4 = Node.new('ananiya')
+node3.next_node = node4
+node5 = Node.new('pioneer')
+node4.next_node = node5
+node6 = Node.new('swaria')
+node5.next_node = node6
+node7 = Node.new('moruapesur')
+node6.next_node = node7
+node8 = Node.new('hilders')
+node7.next_node = node8
+node9 = Node.new('madera')
+node8.next_node = node9
+list = LinkedList.new(node1)
 p list.find_length
 p list.index_of('Gambino')
 p list.read(7)
